f9f76d7019a9c6a903a27a0cfd473a9927af75c5
LuccasRamos <ramosdelucas@gmail.com>
2016-12-13 16:01:50 -0200

    Create ObjetosInvalidos.sql

---
+++ ObjetosInvalidos.sql
@@ -0,0 +1,9 @@
+SELECT      'ALTER '
+         || DECODE (object_type, ' PACKAGE BODY ', ' PACKAGE ', object_type)
+         || ' '
+         || object_name
+         || DECODE (object_type, ' PACKAGE BODY ', ' COMPILE BODY; ', ' COMPILE; ')
+         || CHR(10)
+    FROM user_objects
+   WHERE status = 'INVALID'
+ORDER BY object_name;
