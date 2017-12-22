b60ab2b5c138a258e02bf3b4b1ea29b051f696c9
LuccasRamos <ramosdelucas@gmail.com>
2017-12-22 15:43:11 -0200

    Update EstruturaTabela.sql

--- EstruturaTabela.sql
+++ EstruturaTabela.sql
@@ -1,5 +1,5 @@
-SELECT DISTINCT t1.column_name "Column Name", 
-                t1.column_id "ID",
+/* Formatted on 2017/12/22 15:33 (Formatter Plus v4.8.8) */
+SELECT DISTINCT t1.column_name "Column_Name", t1.column_id "ID",
                 NVL2 ((SELECT c1.column_name
                          FROM user_cons_columns c1, user_constraints a1
                         WHERE a1.table_name = c1.table_name
@@ -10,19 +10,21 @@ SELECT DISTINCT t1.column_name "Column Name",
                       1,
                       ''
                      ) AS "PK",
-                      t1.nullable "Null?",
-                DECODE (t1.data_type,
-                        'VARCHAR2', 'VARCHAR2 (' || char_length || ' Byte)',
-                        'CHAR', 'CHAR (' || char_length || ' Byte)',
-                        'NUMBER', 'NUMBER (' || data_precision || ',' --Implementar quando o valor não for especificado
-                         || data_scale || ')',
-                        data_type
-                       ) "DATA TYPE",
-                       NULL AS "Obrigatório",
-                       NULL AS "Comments",
+                t1.nullable "Null?",
+                DECODE
+                   (t1.data_type,
+                    'VARCHAR2', 'VARCHAR2 (' || char_length || ' Byte)',
+                    'CHAR', 'CHAR (' || char_length || ' Byte)',
+                    'NUMBER', 'NUMBER ('
+                     || data_precision
+                     || ','  --Implementar quando o valor não for especificado
+                     || data_scale
+                     || ')',
+                    data_type
+                   ) "DATA TYPE",
+                 T3.COMMENTS AS "Comentário",
                 DECODE (t1.density, NULL, 'No', 'Yes') "Histogram"
-           FROM user_tab_columns t1, user_cons_columns t2,
-                user_constraints t3
-          WHERE t1.column_name = t2.column_name(+)
-                AND t1.table_name = 'ORCAMENTO_ORC' --Substituir pelo nome da tabela a ser usada
+           FROM user_tab_columns t1, user_col_comments t3
+          WHERE t1.column_name = t3.column_name(+)
+            AND t1.table_name = 'MATBEMSERV_MBS' --substituir o nome da tabela desejada
        ORDER BY "ID"
